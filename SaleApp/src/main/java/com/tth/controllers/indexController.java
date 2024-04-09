088fb90f8e09ae103d0eed6c71d4bc2dd96cfbc3
TongHiep <tonghiep25@gmail.com>
2024-04-09 16:34:09 +0700

    update product

--- SaleApp/src/main/java/com/tth/controllers/indexController.java
+++ SaleApp/src/main/java/com/tth/controllers/indexController.java
@@ -6,10 +6,12 @@ package com.tth.controllers;
 
 import com.tth.services.CategoryService;
 import com.tth.services.ProductService;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -24,9 +26,10 @@ public class indexController {
     private ProductService prodService;
     
     @RequestMapping("/")
-    public String index(Model model){
+    public String index(Model model,
+            @RequestParam Map<String, String> params){
         model.addAttribute("categories", this.cateService.getCates());
-        model.addAttribute('products', this.prodService.getProducts(params));
+        model.addAttribute("products", this.prodService.getProducts(params));
         return "index";
     }
 }
