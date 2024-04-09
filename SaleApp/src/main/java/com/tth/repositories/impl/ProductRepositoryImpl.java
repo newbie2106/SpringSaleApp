07d210ec4dc94c51465e8a6cedeb7e5765d64b38
TongHiep <tonghiep25@gmail.com>
2024-04-09 16:31:25 +0700

    update product

--- SaleApp/src/main/java/com/tth/repositories/impl/ProductRepositoryImpl.java
+++ SaleApp/src/main/java/com/tth/repositories/impl/ProductRepositoryImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.tth.repository.impl;
+package com.tth.repositories.impl;
 
 import com.tth.pojo.Product;
 import com.tth.repositories.ProductRepository;
@@ -16,6 +16,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,41 +28,57 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 @Transactional
 public class ProductRepositoryImpl implements ProductRepository{
-    @Autowired
-    private LocalSessionFactoryBean factory;
-    public List<Product> getProducts(Map<String, String> params) {
+   @Autowired
+   private LocalSessionFactoryBean factory;
+    
+   @Autowired
+   private Environment env;
+   public List<Product> getProducts(Map<String, String> params) {
         Session s = this.factory.getObject().getCurrentSession();
-            CriteriaBuilder b = s.getCriteriaBuilder();
-            CriteriaQuery<Product> q = b.createQuery(Product.class);
-            Root r = q.from(Product.class);
-            q.select(r);
-            
-            List<Predicate> predicates = new ArrayList<>();
-            
-            String kw = params.get("kw");
-            if (kw != null && !kw.isEmpty())
-                predicates.add(b.like(r.get("name"), String.format("%%%s%%", kw)));
-            
-            String fromPrice = params.get("fromPrice");
-            if (fromPrice != null && !fromPrice.isEmpty())
-                predicates.add(b.greaterThanOrEqualTo(r.get("price"), Double.parseDouble(fromPrice)));
-            
-            String toPrice = params.get("toPrice");
-            if (toPrice != null && !toPrice.isEmpty())
-                predicates.add(b.lessThanOrEqualTo(r.get("price"), Double.parseDouble(toPrice)));
-            
-            String cateId = params.get("cateId");
-            if (cateId != null && !cateId.isEmpty())
-                predicates.add(b.equal(r.get("category").as(Integer.class), Integer.parseInt(cateId)));
-            
-            q.where(predicates.toArray(Predicate[]::new));
-            q.orderBy(b.desc(r.get("id")));
-            
-            Query query = s.createQuery(q);
-            List<Product> products = query.getResultList();
-            
-            return products;
-    }
+        CriteriaBuilder b = s.getCriteriaBuilder();
+        CriteriaQuery<Product> q = b.createQuery(Product.class);
+        Root r = q.from(Product.class);
+        q.select(r);
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        String kw = params.get("kw");
+        if (kw != null && !kw.isEmpty()) {
+            predicates.add(b.like(r.get("name"), String.format("%%%s%%", kw)));
+        }
+
+        String fromPrice = params.get("fromPrice");
+        if (fromPrice != null && !fromPrice.isEmpty()) {
+            predicates.add(b.greaterThanOrEqualTo(r.get("price"), Double.parseDouble(fromPrice)));
+        }
+
+        String toPrice = params.get("toPrice");
+        if (toPrice != null && !toPrice.isEmpty()) {
+            predicates.add(b.lessThanOrEqualTo(r.get("price"), Double.parseDouble(toPrice)));
+        }
+
+        String cateId = params.get("cateId");
+        if (cateId != null && !cateId.isEmpty()) {
+            predicates.add(b.equal(r.get("categoryId"), Integer.parseInt(cateId)));
+        }
+
+        q.where(predicates.toArray(Predicate[]::new));
+        q.orderBy(b.desc(r.get("id")));
+
+        Query query = s.createQuery(q);
+        
+         String p = params.get("page");
+        if (p != null && !p.isEmpty()) {
+            int pageSize = Integer.parseInt(env.getProperty("products.pageSize").toString());
+            int start = (Integer.parseInt(p) - 1) * pageSize;
+            query.setFirstResult(start);
+            query.setMaxResults(pageSize);
+        }
+        
+        List<Product> products = query.getResultList();
+
+        return products;
+   }
     
     public void addOrUpdate(Product p) {
          Session s = this.factory.getObject().getCurrentSession();
